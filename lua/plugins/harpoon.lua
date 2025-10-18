05e9df500a5bbc4cf01e371435bc9df96273f6dc
ermenal <ermenal11@gmail.com>
2025-10-18 10:43:28 +0200

    Werkt

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,9 +1,9 @@
 local keys = {
-    { "<M-->", function() require("harpoon"):list():add() end },
-    { "<M-0>", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end },
+    { "<leader>a", function() require("harpoon"):list():add() end },
+    { "<C-e>", function() require("harpoon").ui:toggle_quick_menu(require("harpoon"):list()) end },
 }
 for i = 1, 5, 1 do
-    table.insert(keys, { "<M-" .. i .. ">", function() require("harpoon"):list():select(i) end })
+    table.insert(keys, { "<C-" .. i .. ">", function() require("harpoon"):list():select(i) end })
 end
 return {
     "ThePrimeagen/harpoon",
